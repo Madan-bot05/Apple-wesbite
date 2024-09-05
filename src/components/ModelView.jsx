b233b262b1ba7cc7e3206664c51a1a96a3d323dd
Madan-bot05 <madanmohandas983@gmail.com>
2024-09-05 20:05:27 +0530

    working on the 3D iphone model

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Lights'
+import Iphone from './Iphone'
+import { div } from 'three/examples/jsm/nodes/Nodes.js'
 
-const ModelView = () => {
+const ModelView = ({index,groupRef,gsapType,controlRef,setRotationSize,size,item}) => {
   return (
-    <div>ModelView</div>
+    <View
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-400 w-full h-full
+      ${index===2} ? 'right-[100%]:''`}
+    >
+      <ambientLight intensity={0.3}/>
+      <PerspectiveCamera makeDefault position={[0,0,4]}/>
+      <Lights/>
+      <Suspense fallback={<div>Loading</div>}>
+      <Iphone/>
+      </Suspense>
+    </View>
   )
 }
 
